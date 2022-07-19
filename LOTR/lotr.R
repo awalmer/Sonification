2dd454ee7fe9d6cb374c856e579c83ee623f4213
Aura Walmer <68713406+awalmer@users.noreply.github.com>
2022-07-19 00:29:14 -0700

    Add files via upload

--- LOTR/lotr.R
+++ LOTR/lotr.R
@@ -1,4 +1,4 @@
-## Lord of the Rings Data ##
+## Lord of the Rings Data | Sonification ##
 ## July 11, 2022 ##
 # LINKS: 
 #   [Kaggle Data] https://www.kaggle.com/datasets/paultimothymooney/lord-of-the-rings-data
@@ -13,29 +13,29 @@ library(stringi)
 
 setwd("/Volumes/AuraByte/Data Projects/LOTR/")
 
+# External Data Sources #
 lotr_scripts <- read_csv("Data/lotr_scripts.csv")[,2:4]
 lotr_characters <- read_csv("Data/lotr_characters.csv")
 lotr_char_race <- read_csv("Data/characters_no_surnames.csv")
 
-# Data Cleaning #
+# Data Check #
 name_check <- 
   lotr_scripts %>%
   group_by(char) %>%
   summarise(n_distinct(char))
-# typos in names need to be corrected and duplicates need combining
-# ALERT! lotr_characters data doesn't contain Eowyn, for example! Missing data.
-# Also, data set not in order of lines spoken.
+# Some name text contain typos; duplicates exist; characters missing (e.g. Eowyn)
+# Rows are not in order of lines spoken in script
 
 ## ALTERNATE APPROACH ##
-# Scrape Script from LOTR-FOTR
+# Scrape Script from The Fellowship of the Ring:
 scrape <- read_html("https://imsdb.com/scripts/Lord-of-the-Rings-Fellowship-of-the-Ring,-The.html")
 
-scrape %>%
-  html_elements("b")
-
-scrape_char_line <- 
+scrape_characters <- 
   data.frame(
-  character_lines_in_order = scrape %>% html_elements("b") %>% html_text2()
+    character_lines_in_order = 
+      scrape %>%
+      html_elements("b") %>%
+      html_text2()
 )
 # This gives a data frame of the FIRST body element throughout the script (within each <b></b>),
 # which contains the name of the character saying the line (because a spoken line will always be preceded by character name).
@@ -43,82 +43,72 @@ scrape_char_line <-
 # If I merge this with a comprehensive list of characters from LOTR and keep only matches,
 # I should be able to have a race mapping of character in the order of lines spoken.
 
-# GOAL: Sonification of lines spoken by race
-# different race = different sonic tones
+# GOAL: Sonification of lines spoken, pitch by race
+# (different race = different sonic tones)
 
-# Data Cleaning #
-# scrape_char_line$char_first_name <- gsub( " .*$", "", scrape_char_line$character_lines_in_order) # gets rid of e.g. "(V.O.)" etc.
-scrape_char_line$char_first_name <- ""
-for (row in 1:nrow(scrape_char_line)) {
-  scrape_char_line$char_first_name[row] <- strsplit(scrape_char_line$character_lines_in_order[row], "[ ]")[[1]][1]
-} # just in case two spaces anywhere
-lotr_char_race$name_capitalized <- toupper(lotr_char_race$name) # upper case for matching
-lotr_char_race$name_capitalized_nopunct = stri_trans_general(str = lotr_char_race$name_capitalized, id = "Latin-ASCII") # remove accents etc.
-lotr_char_race <- lotr_char_race[!duplicated(lotr_char_race), ] # 827 to 737 rows, removing duplicates
-# lotr_char_race does not contain GOLLUM!
-lotr_char_race[738,] <- list("Gollum","Hobbit","GOLLUM","GOLLUM")
-lotr_char_race[739,] <- list("Strider","Man","STRIDER","STRIDER")
-lotr_char_race[740,] <- list("Mrs. Sackville Baggins","Hobbit","MRS. SACKVILLE BAGGINS","MRS. SACKVILLE BAGGINS")
 
-unique(scrape_char_line$char_first_name)
-
-## note! weird characters! "\r" -->
-scrape_char_line$char_first_name[23]
-# CREATING A PROBLEM WITH THE JOIN!
-scrape_char_line$char_first_name_cleanup <- gsub( '/(\r\n)+|\r+|\n+/i', '', scrape_char_line$char_first_name)
-# Removed carriage return
-# (thank you https://kaspars.net/blog/regex-remove-all-line-breaks-from-text)
-#scrape_char_line$char_first_name_cleanup <- gsub( ".", "", scrape_char_line$char_first_name_cleanup)
-unique(scrape_char_line$char_first_name_cleanup)
+## DATA CLEANING ##
+# 1. FOTR Script Data:
+scrape_characters$name <- ""
+for (row in 1:nrow(scrape_characters)) {
+  scrape_characters$name[row] <- strsplit(scrape_characters$character_lines_in_order[row], "[ ]")[[1]][1]
+} # Capturing first value before space (name where applicable)
+# Data check:
+unique(scrape_characters$name) # values contain "\r" (carriage return)
+# Remove carriage returns:
+scrape_characters$name_clean <- gsub( '/(\r\n)+|\r+|\n+/i', '', scrape_characters$name)
+# Removed carriage return (thank you https://kaspars.net/blog/regex-remove-all-line-breaks-from-text)
+unique(scrape_characters$name_clean)
 # Add Mrs. Sackville Baggins (one line!)
-scrape_char_line$char_first_name_cleanup[which(scrape_char_line$char_first_name_cleanup=="MRS.")] <- "MRS. SACKVILLE BAGGINS"
+scrape_characters$name_clean[which(scrape_characters$name_clean=="MRS.")] <- "MRS. SACKVILLE BAGGINS"
 
-# Join Datasets #
-lotr_join <- left_join(scrape_char_line, lotr_char_race, by = c("char_first_name_cleanup" = "name_capitalized_nopunct"))
-# okay, now it's looking like a more accurate match.
+# 2. Comprehensive Character Data:
+lotr_char_race$name_clean <- toupper(lotr_char_race$name) # upper case for matching
+lotr_char_race$name_clean = stri_trans_general(str = lotr_char_race$name_clean, id = "Latin-ASCII") # remove accents etc.
+lotr_char_race <- lotr_char_race[!duplicated(lotr_char_race), ] # 827 to 737 rows, removing duplicates
+# Fill in missing characters:
+lotr_char_race[nrow(lotr_char_race)+1,] <- list("Gollum","Hobbit","GOLLUM")
+lotr_char_race[nrow(lotr_char_race)+1,] <- list("Strider","Man","STRIDER")
+lotr_char_race[nrow(lotr_char_race)+1,] <- list("Mrs. Sackville Baggins","Hobbit","MRS. SACKVILLE BAGGINS")
+
+
+## JOIN DATA ##
+lotr_join <- left_join(scrape_characters[c("character_lines_in_order","name_clean")], 
+                       lotr_char_race[c("race","name_clean")],
+                       by = "name_clean") # match looking more accurate
 
 # Drop non-matches:
-lotr_join <- lotr_join[!is.na(lotr_join$race),c("char_first_name_cleanup","race")]
-colnames(lotr_join) <- c("lotr_character_line_in_order","lotr_race")
+lotr_join <- lotr_join[!is.na(lotr_join$race),c("name_clean","race")]
+
+
+# Additional modifications for data sonification purposes:
+lotr_join$order <- 1:nrow(lotr_join)
+# Numericize race (for Two Tone):
+lotr_join$race_num <- as.factor(lotr_join$race)
+levels(lotr_join$race_num) <- list("Ainur"=2,"Elf"=4,"Man"=6,"Dwarf"=8,"Hobbit"=10)
+lotr_join$race_num <- as.numeric(lotr_join$race_num)*2
+# Character ID:
+lotr_join$character_id <- as.numeric(factor(lotr_join$name_clean))
 
 # Write output:
 write_csv(lotr_join, "Output/lotr_fotr_character_line_order.csv")
 
-# New data set for sonification:
-# Number by race
-lotr_line_order_race <- data.frame(race = lotr_join$lotr_race, order = 1:nrow(lotr_join))
-# Write output:
-write_csv(lotr_line_order_race, "Output/lotr_line_order_race.csv")
-write_csv(data.frame(lotr_line_order_race$race), "Output/lotr_line_order_race_onlyrace.csv")
-
-## Looks like with Two Tone, you have to numericize the data of interest?
-lotr_line_order_race_numericized <- data.frame(line_order = 1:nrow(lotr_join), 
-                                               race = lotr_join$lotr_race, race_factorized = as.factor(lotr_join$lotr_race),
-                                               character = lotr_join$lotr_character_line_in_order) 
-levels(lotr_line_order_race_numericized$race_factorized) <- list("Ainur"=2,"Elf"=4,"Man"=6,"Dwarf"=8,"Hobbit"=10)
-lotr_line_order_race_numericized$race_numericized <- as.numeric(lotr_line_order_race_numericized$race_factorized)*2
-lotr_line_order_race_numericized <- lotr_line_order_race_numericized[c(1:2,4,5)]
-
-# add character ID too
-lotr_line_order_race_numericized$character_id <- as.numeric(factor(lotr_join$lotr_character_line_in_order))
-
-write_csv(lotr_line_order_race_numericized, "Output/lotr_line_order_race_numericized.csv")
-
 
 ## How about a data frame of number of lines per character?
-lotr_lines_per_character <- data.frame(character = unique(lotr_join$lotr_character_line_in_order), number_of_lines=0)
-for (n in 1:nrow(lotr_lines_per_character)) {
-  lotr_lines_per_character$number_of_lines[n] <- length(
+lotr_script_summary <- data.frame(character = lotr_join$name_clean, race = lotr_join$race, number_of_lines=0)
+lotr_script_summary <- lotr_script_summary[!duplicated(lotr_script_summary),]
+for (n in 1:nrow(lotr_script_summary)) {
+  lotr_script_summary$number_of_lines[n] <- length(
     which(
-      lotr_join$lotr_character_line_in_order==lotr_lines_per_character$character[n]
+      lotr_join$name_clean==lotr_script_summary$character[n]
       )
     )
 }
   
-lotr_lines_per_character <- left_join(lotr_lines_per_character, distinct(lotr_join), by = c("character"="lotr_character_line_in_order"))
+write_csv(lotr_script_summary, "Output/lotr_lines_per_character.csv")
 
-write_csv(lotr_lines_per_character, "Output/lotr_lines_per_character.csv")
 
+### NOTE: Need to clean up Haldir's elf vs man classification
 
 
 
